6c67315077d5c13250d67d1be57d768fe19e1a40
YihooIsMe <yihoo2016@gmail.com>
2017-12-25 22:59:42 +0800

    add examples

--- public/js/app.js
+++ public/js/app.js
@@ -40,3 +40,25 @@ myApp.controller("secondCtrl",function ($scope) {
         error:false
     }
 });
+
+myApp.controller("thirdCtrl",function ($scope) {
+    $scope.colors = [
+        {name: '黑色', color: 'black', type: "暗色"},
+        {name: '白色', color: 'white', type: "亮色", disabled: false},
+        {name: '红色', color: 'red', type: "暗色", disabled: true},
+        {name: '蓝色', color: 'blue', type: "暗色", disabled: false},
+        {name: '黄色', color: 'yellow', type: "亮色", disabled: true}
+    ];
+
+    //保存选中的状态，默认颜色设置为黑色
+    $scope.colorChosen = $scope.colors[0];
+
+    $scope.countries = {
+        CN: '中国China',
+        US: '美国United States',
+        UK: '英国United Kingdom',
+        GR: '德国Germany'
+    };
+
+    $scope.country = 'CN';
+});
